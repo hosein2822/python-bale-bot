5a72c9b68015b324fd264e778688d1dc96177fa3
hosein2822 <hosein.sh.87.29@gmail.com>
2026-01-02 01:12:31 +0330

    Delete bale directory

--- bale/payments/__init__.py
+++
@@ -1,9 +0,0 @@
-from ._invoice import Invoice
-from ._labeledprice import LabeledPrice
-from ._successfulpayment import SuccessfulPayment
-
-__all__ = (
-    "Invoice",
-    "LabeledPrice",
-    "SuccessfulPayment"
-)
--- bale/request/__init__.py
+++
@@ -1,8 +0,0 @@
-from .parser import ResponseParser
-from .http import HTTPClient, Route
-
-__all__ = (
-	"ResponseParser",
-	"Route",
-	"HTTPClient",
-)
